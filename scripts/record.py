451375ef6501c64925eead2929878be9f59f5ed1
vocdex <nazirjonovsh2000@gmail.com>
2023-02-11 14:58:07 +0900

    replace argparse with hydra

--- scripts/record.py
+++ scripts/record.py
@@ -1,23 +1,26 @@
 """
 Script for capturing individual frames while the camera output is displayed.
+-- First image captured is the background image.
 -- Press SPACEBAR to capture
 -- Press ESC to terminate the program.
 """
-import argparse
 import os
 import os.path
-
+import hydra
 import cv2
-
+from pathlib import Path
 from digit_depth.digit.digit_sensor import DigitSensor
 
-base_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+base_path = Path(__file__).parent.parent.resolve()
 
 
-def record_frame(digit_sensor, dir_name: str):
+@hydra.main(config_path=f"{base_path}/config", config_name="digit.yaml", version_base=None)
+def record_frame(cfg):
+    digit_sensor = DigitSensor(cfg.sensor.fps, cfg.sensor.resolution, cfg.sensor.serial_num)
+    dir_name = create_dir("images")
     img_counter = len(os.listdir(dir_name))
     digit_call = digit_sensor()
-    serial_num = digit_call.serial
+    capture_background = True
     while True:
         frame = digit_call.get_frame()
         cv2.imshow(f"{digit_call.serial}", frame)
@@ -28,24 +31,28 @@ def record_frame(digit_sensor, dir_name: str):
             break
         elif k % 256 == 32:
             # SPACEBAR hit
-            img_name = "{}/frame_{:0>1}.png".format(dir_name, img_counter)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            img_counter += 1
+            if capture_background:
+                img_name = "{}/background.png".format(dir_name)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                capture_background = False
+                print("Background image captured.")
+            else:
+                img_name = "{}/frame_{:0>1}.png".format(dir_name, img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                img_counter += 1
     cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("--fps", type=int, default=30, help="Frames per second. Max:60 on QVGA")
-    argparser.add_argument("--resolution", type=str, default="QVGA", help="QVGA, VGA")
-    argparser.add_argument("--serial_num", type=str, default="D00003", help="Serial number of DIGIT")
-    args = argparser.parse_args()
+def create_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+        print("Directory {} created for saving images".format(dir_name))
+    else:
+        print("Directory {} already exists".format(dir_name))
+    return dir_name
 
 
-    if not os.path.exists(os.path.join(base_path, "images")):
-        os.makedirs(os.path.join(base_path, "images"), exist_ok=True)
-        print("Directory {} created for saving images".format(os.path.join(base_path, "images")))
-    digit = DigitSensor(args.fps, args.resolution, args.serial_num)
-
-    record_frame(digit, os.path.join(base_path, "images"))
+if  __name__ == "__main__":
+    record_frame()
